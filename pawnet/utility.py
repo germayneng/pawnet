797151ed0242978bfce24cc7e23548db2a8dc3b2
germayneng <germayneng@hotmail.com>
2021-12-03 12:33:35 +0000

    Update week3 - Added utility to include pawNetAdvance, mixup exp, swin large model and custom average models

--- pawnet/utility.py
+++ pawnet/utility.py
@@ -26,9 +26,12 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning import LightningDataModule, LightningModule
 
+
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.optim.swa_utils import AveragedModel, SWALR
+
 from timm import create_model
 
 # def seed_everything(seed=1234):
@@ -132,11 +135,11 @@ class PetfinderDataModule(LightningDataModule):
 
     def train_dataloader(self):
         train_data = pawnetDataset(annotation_df=self._train_df,img_dir = self.img_dir ,transform = self.train_transformation) # can set custom len to let model exceed training size (since we are augmenting)
-        return torch.utils.data.DataLoader(train_data,batch_size=self.batch_size,num_workers =self.num_workers, shuffle=True)
+        return torch.utils.data.DataLoader(train_data,batch_size=self.batch_size,num_workers =self.num_workers, shuffle=True, pin_memory=False)
 
     def val_dataloader(self):
         val_data = pawnetDataset(annotation_df=self._val_df,img_dir = self.img_dir, transform = self.test_transformation)
-        return torch.utils.data.DataLoader(val_data,batch_size=self.batch_size,num_workers =self.num_workers, shuffle=False)
+        return torch.utils.data.DataLoader(val_data,batch_size=self.batch_size,num_workers =self.num_workers, shuffle=False, pin_memory=False)
 
 
 
@@ -237,15 +240,21 @@ class pawNetBasic(pl.LightningModule):
         labels = labels.float() / 100.0
         
         # check for mixup and only for train
+        # autocast for more effective training / memory usage
+        # https://effectivemachinelearning.com/PyTorch/8._Faster_training_with_mixed_precision
+        # https://pytorch.org/docs/stable/notes/amp_examples.html
+        # https://www.kaggle.com/c/petfinder-pawpularity-score/discussion/291159
 
         if (self.mixup_alpha is not None) & (mode == "train"):
             # perform mixup
-            x,y = mixup(images,labels,alpha=self.mixup_alpha)
-            logits = self.forward(x).squeeze(1)
-            loss = self._criterion(logits, y)
+            x,y = mixup(images,labels,alpha=self.mixup_alpha,device= self.device)
+            with torch.cuda.amp.autocast(enabled=True):
+                logits = self.forward(x).squeeze(1)
+                loss = self._criterion(logits, y)
         else:
-            logits = self.forward(images).squeeze(1)
-            loss = self._criterion(logits, labels)
+            with torch.cuda.amp.autocast(enabled=True):
+                logits = self.forward(images).squeeze(1)
+                loss = self._criterion(logits, labels)
         
         # return logloss for training mode, scaled for others
         pred = logits.sigmoid().detach().cpu() * 100.
@@ -336,6 +345,7 @@ def mixup(images,labels,alpha=0.2, device = "cuda"):
     """
     # shuffle images
     # https://discuss.pytorch.org/t/shuffling-a-tensor/25422/9
+    # https://pytorch-lightning.readthedocs.io/en/latest/advanced/multi_gpu.html type_as to allow multi gpu
     indices = torch.randperm(len(images)).to(device)
     new_images = images[indices].view(images.size())
     
@@ -353,3 +363,227 @@ def mixup(images,labels,alpha=0.2, device = "cuda"):
     y = labels * ty + new_labels * (1-ty)
 
     return x,y
+
+
+class pawNetAdvance(pl.LightningModule):
+    """
+    Advance version with manual optimization.
+    It uses manual optimization to allow custom
+    optimzer and scheduler step. This is needed in order
+    to perform model weights averaging
+
+    Custom optimizer/scheduler step:
+    https://pytorch-lightning.readthedocs.io/en/latest/common/optimizers.html#gradient-accumulation
+
+
+    SWA / EMW weights
+    https://pytorch.org/blog/pytorch-1.6-now-includes-stochastic-weight-averaging/
+    https://forums.pytorchlightning.ai/t/stochastic-weight-averaging/400
+
+    Current epoch
+    https://github.com/PyTorchLightning/pytorch-lightning/issues/1424
+
+    """
+    
+    
+    def __init__(self,criterion, model_config):
+        """
+
+        Parameters
+        ----------
+        criterion : [type]
+            torch loss criterion
+        model_config : 
+            attrDict object from base_config_manager.load_config().XXmodel
+        dropout : float, optional
+            dropout, by default 0.5
+        lr : float, optional
+            learning rate, by default 0.00001
+        """
+        super().__init__()
+        self.dropout = model_config.dropout
+        self.lr = model_config.learning_rate
+        self._criterion = criterion        
+        # initialize layers
+        # https://fastai.github.io/timmdocs/tutorial_feature_extractor
+        # remove FCL by setting num_classes=0
+        self.pretrained = create_model(
+            model_config.pretrained, 
+            pretrained=True, 
+            num_classes=0, 
+            in_chans=3
+        )
+        self.model_config = model_config
+
+        # Important: This property activates manual optimization.
+        self.automatic_optimization = False
+
+        # get mixup parameter
+        if self.model_config.mixup is not None:
+            print("will perform mixup")
+            self.mixup_alpha = self.model_config.mixup.alpha
+        else:
+            self.mixup_alpha = None
+        
+        # determine if to average =====
+        # https://forums.pytorchlightning.ai/t/stochastic-weight-averaging/400
+        # https://pytorch.org/blog/pytorch-1.6-now-includes-stochastic-weight-averaging/
+        if self.model_config.average.type == "swa":
+            self.wa_model = AveragedModel(self)
+            self.wa_start = self.model_config.average.start
+        elif self.model_config.average.type == "ema":
+            ema_avg = lambda averaged_model_parameter, model_parameter, num_averaged: 0.01 * averaged_model_parameter + 0.99 * model_parameter
+            self.wa_model = AveragedModel(self, avg_fn=ema_avg)
+            self.wa_start = self.model_config.average.start
+        elif self.model_config.average is None:
+            self.wa_model = None
+            self.wa_start = None
+        
+        
+        
+        # create layers based on pretrained model selected (this affects the feature map size)
+        # self.global_avg_pooling = torch.nn.AdaptiveAvgPool2d(1) # timm pretrain comes with pooling    
+        self.linear_1 = torch.nn.Linear(in_features=self.model_config.feature_map_out_size,out_features=1000)
+        self.prelu = torch.nn.PReLU()
+        self.linear_2 = torch.nn.Linear(in_features=1000,out_features=1)
+        
+    def forward(self,x):
+        out = self.pretrained(x)
+#         out = out.view(out.size(0), -1) # reshape for linear. timm already returns with CHW flatten
+        out = torch.nn.Dropout(self.dropout)(out)
+        out = self.linear_1(out)
+        out = self.prelu(out)
+        out = self.linear_2(out)
+        
+        return out
+    
+    
+    def training_step(self, batch, batch_idx):
+        """
+        logic instead batch loop
+        """
+        # retrieve optimizers and schedulers
+        opt = self.optimizers()
+        opt.zero_grad()
+        
+        
+        loss, pred, labels = self.__share_step(batch, 'train')
+        self.manual_backward(loss)
+        opt.step()
+        
+        return {'loss': loss, 'pred': pred, 'labels': labels}
+        
+    def validation_step(self, batch, batch_idx):
+        """
+        logic instead batch loop for validation
+        """
+        
+        loss, pred, labels = self.__share_step(batch, 'val')
+        return {'loss': loss, 'pred': pred, 'labels': labels}
+    
+    def __share_step(self, batch, mode):
+        images, labels = batch
+        labels = labels.float() / 100.0
+        
+        # check for mixup and only for train
+        # autocast for more effective training / memory usage
+        # https://effectivemachinelearning.com/PyTorch/8._Faster_training_with_mixed_precision
+        # https://pytorch.org/docs/stable/notes/amp_examples.html
+        # https://www.kaggle.com/c/petfinder-pawpularity-score/discussion/291159
+
+        if (self.mixup_alpha is not None) & (mode == "train"):
+            # perform mixup
+            x,y = mixup(images,labels,alpha=self.mixup_alpha,device= self.device)
+            with torch.cuda.amp.autocast(enabled=True):
+                logits = self.forward(x).squeeze(1)
+                loss = self._criterion(logits, y)
+        else:
+            with torch.cuda.amp.autocast(enabled=True):
+                logits = self.forward(images).squeeze(1)
+                loss = self._criterion(logits, labels)
+        
+        # return logloss for training mode, scaled for others
+        pred = logits.sigmoid().detach().cpu() * 100.
+        labels = labels.detach().cpu() * 100.
+        return loss, pred, labels
+        
+    def training_epoch_end(self, outputs):
+        """
+        called every end of epoch, contains logic
+        at end of epoch
+        """
+        
+
+        # update scheduler , parameters
+        
+        # sch = self.lr_schedulers()[0]
+        swa_sch = self.lr_schedulers()
+        if self.wa_model is not None:
+            if self.current_epoch > self.wa_start:
+                self.wa_model.update_parameters(self)
+                swa_sch.step()
+        # else:
+        #     sch.step()
+
+        self.__share_epoch_end(outputs, mode = 'train')
+
+
+    def validation_epoch_end(self, outputs):
+        self.__share_epoch_end(outputs, mode = 'val')    
+        
+    def __share_epoch_end(self, outputs, mode):
+        """
+        output is a list of output defined in
+        `training_step` as well as `validation_step`.
+        Need to iterate through each iteration's output.
+        the output was a dict
+        """
+        preds = []
+        labels = []
+        losses = []
+        for out in outputs:
+            pred, label, loss = out['pred'], out['labels'], out["loss"]
+            preds.append(pred)
+            labels.append(label)
+            losses.append(loss.view(-1,1))
+        preds = torch.cat(preds)
+        labels = torch.cat(labels)
+        losses = torch.cat(losses)
+        if mode == "train":
+            loglogss_metrics = losses.mean() # average logloss across iterations
+            self.log(f'{mode}_logloss', loglogss_metrics, prog_bar=True)
+        else:
+            print(f"{mode}: skip logging for logloss")
+            
+        # RMSE
+        metrics = torch.sqrt(((labels - preds) ** 2).mean())
+        # https://pytorch-lightning.readthedocs.io/en/stable/extensions/logging.html
+        # automatic accumulation at end of epoch for training, true always for test,validation loops
+        self.log(f'{mode}_RMSE_loss', metrics, prog_bar=True)
+        
+        
+    def configure_optimizers(self):
+        """
+        https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.core.lightning.html#pytorch_lightning.core.lightning.LightningModule.configure_optimizers
+        
+        Any of these 6 options.
+
+        Single optimizer.
+
+        List or Tuple of optimizers.
+
+        Two lists - The first list has multiple optimizers, and the second has multiple LR schedulers (or multiple lr_scheduler_config).
+
+        Dictionary, with an "optimizer" key, and (optionally) a "lr_scheduler" key whose value is a single LR scheduler or lr_scheduler_config.
+
+        Tuple of dictionaries as described above, with an optional "frequency" key.
+
+        None - Fit will run without any optimizer.
+        """
+        #opt = torch.optim.Adam(self.parameters())
+        # TODO: add learning rate to config
+        opt = torch.optim.AdamW(self.parameters(),lr=self.lr)
+        # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(opt,T_0=20,eta_min=1e-4)
+        swa_scheduler = SWALR(opt,anneal_strategy="linear", anneal_epochs=5, swa_lr=0.05)
+  
+        return [opt], [swa_scheduler]
